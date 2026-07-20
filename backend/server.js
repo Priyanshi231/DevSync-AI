cb562b68423c0f9a7094e6dc6a6e6042dfaaface
Priyanshi <priyanshjaini1666@gmail.com>
2026-07-20 21:08:34 +0530

    database setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
+
 import http from 'http';
 import app from './app.js';
+import connectDB from './db/db.js';
+
+connectDB();
 
 const port = process.env.PORT || 3000;
 
