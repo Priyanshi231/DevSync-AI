619b933a1cf2b19a0e50e3cd3fee8e5640a91e22
Priyanshi <priyanshjaini1666@gmail.com>
2026-08-18 22:59:47 +0530

    frontend socket setup

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ connectDB();
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+  cors: {
+    origin: '*',
+  }
+});
 
 
 io.use((socket, next) => {
